7953f9b27dfc55856a809c2bada8f21292f11bd5
Natalia Bryzgalova <bryzgalova_n@mail.ru>
2020-05-08 23:32:00 +0500

    Initial commit

---
+++ src/test/java/ru/netology/MobileBankApiTest.java
@@ -0,0 +1,25 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+
+class MobileBankApiTestV1 {
+    @Test
+    void shouldPostMan() {
+      // Given - When - Then
+      // Предусловия
+      given()
+          .baseUri("https://postman-echo.com")
+          .body("balance")
+      // Выполняемые действия
+      .when()
+           .post("/post")
+      // Проверки
+      .then()
+          .statusCode(200)
+          .body("balance")
+        ;
+    }
+}
